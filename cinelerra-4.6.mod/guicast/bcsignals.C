5f709d7d9ee401dd945301432bde747d50c5b704
Good1.2Guy <good1.2guy@gmail.com>
2015-04-01 08:57:18 -0600

    repair x_error_handler err msg reporting

--- cinelerra-4.6.mod/guicast/bcsignals.C
+++ cinelerra-4.6.mod/guicast/bcsignals.C
@@ -495,7 +495,7 @@ int BC_Signals::x_error_handler(Display *display, XErrorEvent *event)
 {
 	char string[1024];
 	XGetErrorText(event->display, event->error_code, string, 1024);
-	fprintf(stderr, "BC_Resources::x_error_handler: error_code=%d opcode=%d,%d %s\n",
+	fprintf(stderr, "BC_Signals::x_error_handler: error_code=%d opcode=%d,%d %s\n",
 		event->error_code, event->request_code, event->minor_code, string);
 	return 0;
 }
